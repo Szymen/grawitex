aad38f2470db25d6e0846583613463ecce1f6f62
Adam Olękiewicz <adamolekiewicz@gmail.com>
2017-04-19 10:52:58 +0200

    Rebuilt directory tree (nothing works yet).

--- EAgeneratedFiles/DataTypeObjects.c
+++
@@ -1,8 +0,0 @@
-/****************************************************
- *  DataTypeObjects.c                                         
- *  Created on: 25-mar-2017 02:06:28                      
- *  Implementation of the Class DataTypeObjects       
- *  Original author: szym                     
- ****************************************************/
-
-#include "DataTypeObjects.h"
--- EAgeneratedFiles/Main.c
+++
@@ -1,8 +0,0 @@
-/****************************************************
- *  Main.c                                         
- *  Created on: 25-mar-2017 02:06:28                      
- *  Implementation of the Class Main       
- *  Original author: szym                     
- ****************************************************/
-
-#include "Main.h"
